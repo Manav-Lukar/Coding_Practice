56094ce13f535d59d5901a7f86f538deded961ee
Manav Lukar <91900036+Manav-Lukar@users.noreply.github.com>
2023-02-01 10:11:27 +0530

    Add files via upload

---
+++ KNGTOR.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    int sum1 = a*5;
+	    int sum2 = b*7;
+	    int sum3 = sum1+sum2;
+	    cout<<sum3<<endl;
+	}
+	return 0;
+}
